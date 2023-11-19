39cddd970ed6787c314b28599903c7eec47ee1c6
zestsystem <mikeyim@unitetheculture.org>
2023-11-18 20:16:06 -0800

    learn(ch1): Creating Local Variables Using Let Bindings

--- Chapter1/Main.hs
+++ Chapter1/Main.hs
@@ -1,11 +1,32 @@
 module Main where
 
 makeGreeting salutation person =
-  salutation <> " " <> person
+  let messageWithTrailingSpace = salutation <> " "
+   in messageWithTrailingSpace <> person
 
-greetPerson = makeGreeting "Hello"
+extendedGreeting person =
+  let joinWithNewlines a b = a <> "\n" <> b
+      helloAndGoodbye hello goodbye =
+        let hello' = makeGreeting hello person
+            goodbye' = makeGreeting goodbye person
+         in joinWithNewlines hello' goodbye'
+   in helloAndGoodbye "Hello" "Goodbye"
 
-enthusiasticGreeting salutation =
-  makeGreeting (salutation <> "!")
+letWhereGreeting name place =
+  let salutation = "Hello" <> name
+      meetingInfo = location "Tuesday"
+   in salutation <> " " <> meetingInfo
+  where
+    location day = "we met at " <> place <> " on a " <> day
 
-main = print "no salutation to show yet"
+extendedGreeting' person =
+  helloAndGoodbye "Hello" "Goodbye"
+  where
+    helloAndGoodbye hello goodbye =
+      joinWithNewlines hello' goodbye'
+      where
+        hello' = makeGreeting hello person
+        goodbye' = makeGreeting goodbye person
+    joinWithNewlines a b = a <> "\n" <> b
+
+main = print $ makeGreeting "Hello" "George"
