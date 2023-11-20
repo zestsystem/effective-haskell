48692fb984f9896dfce06ce446d746d67fe3d406
zestsystem <mikeyim@unitetheculture.org>
2023-11-19 23:17:02 -0800

    exercise(ch1): The Fibonacci Sequence

--- Chapter1/Exercises.hs
+++ Chapter1/Exercises.hs
@@ -4,3 +4,9 @@ module Exercises where
 factorial n
   | n == 1 = n
   | otherwise = n * factorial (n - 1)
+
+-- The Fibonacci Sequence
+fibonacci n
+  | n == 0 = 0
+  | n == 1 = 1
+  | otherwise = fibonacci (n - 2) + fibonacci (n - 1)
