34fa88d1350a05f4e1c908d96bd235aaf656053e
zestsystem <mikeyim@unitetheculture.org>
2023-11-25 16:45:39 -0800

    learn(ch2): Destructuring Values with Pattern Matching

---
+++ Chapter2/DestructuringValuesWithPatternMatching.hs
@@ -0,0 +1,22 @@
+module DestructuringValuesWithPatternMatching where
+
+customGreeting "George" = "Oh, hey George!"
+customGreeting name = "Hello, " <> name
+
+-- pattern matching variety!
+matchNumber 0 = "zero"
+matchNumber n = show n
+
+matchList [1, 2, 3] = "one, two, three"
+matchList list = show list
+
+matchTuple ("hello", "world") = "greetings"
+matchTuple tuple = show tuple
+
+matchBool True = "yep"
+matchBool bool = "this must be false"
+
+matchTuple' ("hello", "world") = "Hello there, you great big world"
+matchTuple' ("hello", name) = "Oh hi there, " <> name
+matchTuple' (salutation, "George") = "Oh! " <> salutation <> " George"
+matchTuple' n = show n
