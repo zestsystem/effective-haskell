0307227366da7f3b9c1cfe136a475777fc98f45d
zestsystem <mikeyim@unitetheculture.org>
2023-11-26 21:30:19 -0800

    exercise(ch2): Zipping Lists

--- Chapter2/Exercises.hs
+++ Chapter2/Exercises.hs
@@ -6,3 +6,9 @@ reverseWithFoldl lst =
 
 reverseWithFoldr lst =
   foldr (\x f acc -> f (x : acc)) id lst []
+
+-- Zipping List
+zipWithNoListComprehension f (x : xs) (y : ys) = f x y : zipWithNoListComprehension f xs ys
+zipWithNoListComprehension _ _ _ = []
+
+zipWithListComprehension f xs ys = [f x y | (x, y) <- zip xs ys]
