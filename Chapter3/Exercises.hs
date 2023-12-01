0c9f9506ba1d16c8b1f3b0192ba33607af08fd20
zestsystem <mikeyim@unitetheculture.org>
2023-11-30 20:11:36 -0800

    exercise(ch3): Undefined

---
+++ Chapter3/Exercises.hs
@@ -0,0 +1,10 @@
+module Chapter3.Exercises where
+
+-- Undefined
+-- My Response: Because undefined works for all types and function is also a type.
+-- Answer: why are there so many different ways to define an expression using undefined”.
+--         Since undefined can be used anywhere, for an expression of any type, it’s extremely flexible.
+--         You can use undefined almost anywhere, to fill in for almost anything, even things that wouldn’t ever make sense with real code.
+--         That’s one of the drawbacks of this technique.
+--         When you allow the compiler to infer the type of undefined, you may find that you’re getting a false sense of security when your program compiles.
+--         It’s useful frequently enough that you shouldn’t necessarily avoid it altogether, but beware of the drawbacks.
